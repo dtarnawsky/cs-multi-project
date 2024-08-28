b6f4566402f5b401e891f7268371786310fda818
damian.ionic <damian@ionic.io>
2024-08-28 09:16:23 -0700

    feat: show token reading

--- cs-multi-shell/src/app/home/home.page.ts
+++ cs-multi-shell/src/app/home/home.page.ts
@@ -13,8 +13,18 @@ export class HomePage {
   private authenticationService = inject(AuthenticationService);
   constructor() { }
 
-  open(app: string) {
+  async open(app: string) {
+    const token = await this.authenticationService.decodeToken();
+    // Token will be something like:
+    // {sub: '00u1n5bb0ozhADQaL0h8', name: 'CustomerSuccess Support', email: 'support@ionic.io', ver: 1, iss: 'https://dev-622807.oktapreview.com', …}
+    console.log('token', token);
+
+    // Options for passing tokens to other apps include:
+    // 1. Adding it the parameters of the URL
+    // 2. The app using Identity Vault and reading the token from the vault
+    // 3. Storing temporarily in session storage, then the app deleting it from session storage
     location.href = `/${app}/index.html`;
+
   }
 
   logout() {
